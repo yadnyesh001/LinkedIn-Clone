dbb9eeba72065ce1c7f7661d709bcc2d277faeeb
Yadnyesh001 <yadnyeshbadgujar2004@gmail.com>
2025-01-07 20:25:54 +0530

    notification email

--- backend/emails/emailHandlers.js
+++ backend/emails/emailHandlers.js
@@ -1,23 +1,26 @@
 import { mailtrapClient, sender } from "../lib/mailtrap.js";
-import { createWelcomeEmailTemplate } from "./emailTemplates.js";
+import {
+	createCommentNotificationEmailTemplate,
+	createConnectionAcceptedEmailTemplate,
+	createWelcomeEmailTemplate,
+} from "./emailTemplates.js";
 
 export const sendWelcomeEmail = async (email, name, profileUrl) => {
-  const recipient = [{ email }]
+	const recipient = [{ email }];
 
-  try {
-    const response = await mailtrapClient.send({
-      from: sender,
-      to: recipient,
-      subject: "Welcome to UnLinked",
-      html: createWelcomeEmailTemplate(name, profileUrl),
-      category: "Welcome "
-    });
+	try {
+		const response = await mailtrapClient.send({
+			from: sender,
+			to: recipient,
+			subject: "Welcome to UnLinked",
+			html: createWelcomeEmailTemplate(name, profileUrl),
+			category: "welcome",
+		});
 
-    console.log("Welcome Email sent successfully", response);
-
-  } catch (error) {
-    throw new Error("Error sending Welcome Email", error);
-  }
+		console.log("Welcome Email sent succesffully", response);
+	} catch (error) {
+		throw error;
+	}
 };
 
 export const sendCommentNotificationEmail = async (
@@ -41,4 +44,18 @@ export const sendCommentNotificationEmail = async (
 	} catch (error) {
 		throw error;
 	}
+};
+
+export const sendConnectionAcceptedEmail = async (senderEmail, senderName, recipientName, profileUrl) => {
+	const recipient = [{ email: senderEmail }];
+
+	try {
+		const response = await mailtrapClient.send({
+			from: sender,
+			to: recipient,
+			subject: `${recipientName} accepted your connection request`,
+			html: createConnectionAcceptedEmailTemplate(senderName, recipientName, profileUrl),
+			category: "connection_accepted",
+		});
+	} catch (error) {}
 };
