8877fad91ca5e7d7e263dbb669e5088261243a06
Yadnyesh001 <yadnyeshbadgujar2004@gmail.com>
2025-01-08 18:24:41 +0530

    connection route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
+import notificationRoutes from './routes/notification.route.js';
+import connectionRoutes from "./routes/connection.route.js";
 
 import { connectDB } from './lib/db.js';
 
@@ -13,12 +15,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" })); 
 app.use(cookieParser());
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/posts', postRoutes);
+app.use('/api/v1/notifications', notificationRoutes);
+app.use("/api/v1/connections", connectionRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
